fb927e29c5b2c41215a42ded84f4041ad613d169
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-03-19 11:29:12 +0530

    Base File str. Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Dashboard from './pages/Dashboard'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import Menubar from './components/Menubar'
 
 const App = () => {
   return (
     <div className='text-center'>
-      App
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/product' element={<Product />} />
+        </Routes>
+      <Menubar />
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/assets/assets.js
@@ -0,0 +1,65 @@
+import calender_icon from './images/calender_icon.png';
+import contact_icon from './images/contact_icon.png';
+import dashboard_icon from './images/dashboard_icon.png';
+import drop_down from './images/drop_down.png';
+import favourite_icon from './images/favourite_icon.png';
+import graph from './images/graph.png';
+import inbox_icon from './images/inbox_icon.png';
+import invoice_icon from './images/invoice_icon.png';
+import logout_icon from './images/logout_icon.png';
+import menu_bar from './images/menu_bar.png';
+import notification_icon from './images/notification_icon.png';
+import order_list from './images/order_list.png';
+import pricing_icon from './images/pricing_icon.png';
+import product_icon from './images/product_icon.png';
+import product_stock from './images/product_stock.png';
+import search_icon from './images/search_icon.png';
+import setting_icon from './images/setting_icon.png';
+import table_icon from './images/table_icon.png';
+import team_icon from './images/team_icon.png';
+import todo_icon from './images/todo_icon.png';
+import total_orders from './images/total_orders.png';
+import total_pending from './images/total_pending.png';
+import total_sales from './images/total_sales.png';
+import total_users from './images/total_users.png';
+import ui_icon from './images/ui_icon.png';
+import uk_flag from './images/uk_flag.png';
+import units_bar from './images/units_bar.png';
+import user_icon from './images/user_icon.png';
+
+
+
+export const assets = {
+    calender_icon,
+    contact_icon,
+    dashboard_icon,
+    drop_down,
+    favourite_icon,
+    graph,
+    invoice_icon,
+    inbox_icon,
+    logout_icon,
+    menu_bar,
+    notification_icon,
+    order_list,
+    pricing_icon,
+    product_icon,
+    product_stock,
+    search_icon,
+    setting_icon,
+    table_icon,
+    team_icon,
+    todo_icon,
+    total_orders,
+    total_pending,
+    total_sales,
+    total_users,
+    ui_icon,
+    uk_flag,
+    units_bar,
+    user_icon
+}
+
+
+
+
