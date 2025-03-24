9bc9701b3c726baa3da2926b972a678e1106e176
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-03-24 10:30:58 +0530

    Icons Adjusted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,24 +23,30 @@ const Navbar = ({ menuBar }) => {
                     />
                 </div>
             </div>
+            <div className='flex'></div>
+            <div className='flex'></div>
+            <div className='flex'></div>
+            <div className='flex'></div>
+            <div className='flex'></div>
+            <div className='flex'></div>
             <div className='flex items-center space-x-6'>
                 <img src={assets.notification_icon} alt="notification_icon" className='h-6 w-6 cursor-pointer' />
 
                 <div className='hidden md:flex items-center space-x-2 cursor-pointer'>
-                    <img src={assets.uk_flag} alt="uk_flag" className='h-5 w-8 rounded-sm' />
+                    <img src={assets.uk_flag} alt="uk_flag" className='rounded-sm' />
                     <div className='flex items-center'>
                         <p className='text-sm'>English</p>
-                        <img src={assets.drop_down} alt="dropdown" className='h-3 w-4 ml-1' />
+                        <img src={assets.drop_down} alt="dropdown" className='ml-1' />
                     </div>
                 </div>
 
                 <div className='flex items-center space-x-2 cursor-pointer'>
                     <img src={assets.user_icon} alt="user_icon" className='h-8 w-8 rounded-full' />
                     <div className='hidden md:flex flex-col items-start'>
-                        <p className='text-sm font-medium'>Moni</p>
+                        <p className='text-sm font-medium'>User</p>
                         <p className='text-xs text-gray-400'>Admin</p>
                     </div>
-                    <img src={assets.drop_down} alt="dropdown" className='h-3 w-4' />
+                    <img src={assets.drop_down} alt="dropdown" className='ml-1' />
                 </div>
             </div>
         </div>
